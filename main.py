8f63b19bd3bea45fbf0cc730441faccc898d69ef
Discount Surgeon <JMor0707@gmail.com>
2021-01-24 12:49:04 -0600

    Adding Twitter integration back in

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import requests
 import os
 import json
 from requests_oauthlib import OAuth1Session
+import urllib
+
+# Kivy imports -- do not
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -11,45 +14,55 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.scrollview import ScrollView
 
 # Get secrets
-# client_key = os.environ['CLIENT_KEY']
-# client_secret = os.environ['CLIENT_SECRET']
-# resource_owner_key = os.environ['RESOURCE_OWNER_KEY']
-# resource_owner_secret = os.environ['RESOURCE_OWNER_SECRET']
+client_key = os.environ['CLIENT_KEY']
+client_secret = os.environ['CLIENT_SECRET']
+resource_owner_key = os.environ['RESOURCE_OWNER_KEY']
+resource_owner_secret = os.environ['RESOURCE_OWNER_SECRET']
 
 
 # The main widget for the main menu Kivy app
 class MainWidget(Widget):
+    def __init__(self, session):
+        super().__init__()
+        self.session = session
+
+    # Send a request to the endpoint using our OAuth V1.0 session
+    def post_tweet(self, content):
+        urlsafe = urllib.parse.urlencode({'status': content})
+        url = "https://api.twitter.com/1.1/statuses/update.json?{}".format(urlsafe)
+        response = self.session.post(url)
+        print(response)
+        return response
+
+    def get_tweets(self):
+        url = "https://api.twitter.com/2/tweets/1351290770804920321?tweet.fields=created_at,attachments&expansions=author_id"
+        response = self.session.get(url)
+        print(response)
+        return response
+
     def on_click(self, tw_btn):
         self.tw_showbox.text = self.tw_tbox.text
 
     def refresh(self):
+        self.get_tweets()
         self.tw_showbox.text = "fresh af"
 
 
 # The main menu for the Kivy app
 class MainMenu(App):
+    # Creates a new OAuth V1.0 session for accessing Twitter
+    def get_session(self):
+        # Create OAuth V1.0 session
+        twitter = OAuth1Session(client_key=client_key,
+                                client_secret=client_secret,
+                                resource_owner_key=resource_owner_key,
+                                resource_owner_secret=resource_owner_secret)
+        return twitter
+
     def build(self):
-        return MainWidget()
+        tw_session = self.get_session()
+        return MainWidget(tw_session)
 
-
-# Creates a new OAuth V1.0 session for accessing Twitter
-def get_session():
-    # Create OAuth V1.0 session
-    twitter = OAuth1Session(client_key=client_key,
-                            client_secret=client_secret,
-                            resource_owner_key=resource_owner_key,
-                            resource_owner_secret=resource_owner_secret)
-    return twitter
-
-
-# tw_session = get_session()
-# # Send a request to the endpoint using our OAuth V1.0 session
-# # url = 'https://api.twitter.com/1.1/account/settings.json'
-# url = "https://api.twitter.com/1.1/statuses/update.json?status=Hello%20again%20World%21"
-# response = tw_session.post(url)
-#
-# # Print response
-# print(response.text)
 
 if __name__ == '__main__':
     MainMenu().run()
