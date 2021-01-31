ba2d930ff98eca71940b3ad497f3026189f0b0d4
Discount Surgeon <JMor0707@gmail.com>
2021-01-30 22:54:16 -0600

    Added OAuth login functionality methods to main.py and some placeholder resources to MainMenu.kv-- the current login functionality works, but as a CLI workflow rather than the ideal Kivy-based GUI.

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import os
 import json
 from requests_oauthlib import OAuth1Session
 import urllib
+import os
+import webbrowser
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 # Kivy imports -- do not
 from kivy.app import App
@@ -16,8 +19,60 @@ from kivy.uix.scrollview import ScrollView
 # Get secrets
 client_key = os.environ['CLIENT_KEY']
 client_secret = os.environ['CLIENT_SECRET']
-resource_owner_key = os.environ['RESOURCE_OWNER_KEY']
-resource_owner_secret = os.environ['RESOURCE_OWNER_SECRET']
+# resource_owner_key = os.environ['RESOURCE_OWNER_KEY']
+# resource_owner_secret = os.environ['RESOURCE_OWNER_SECRET']
+
+
+# TODO maybe wrap these authentication methods in a class
+# Initial "leg" of the OAuth 3-legged auth method. Get a two keys from Twitter API for delivering users
+# to a YIT-V login browser window
+def get_resource_token():
+    request_session = OAuth1Session(client_key=client_key, client_secret=client_secret)
+    url = 'https://api.twitter.com/oauth/request_token'
+    response = request_session.get(url)
+    response_token = response.text.split('&')
+    r_key = response_token[0].split('=')[1]
+    r_secret = response_token[1].split('=')[1]
+    return [r_key, r_secret]
+
+
+# Redirect user to Twitter to log in and authorize YIT-V
+def get_oauth_verifier(owner_key):
+    url = "https://api.twitter.com/oauth/authenticate?oauth_token={}".format(owner_key)
+    webbrowser.open(url=url)
+
+
+# Use the OAuth verifier from above method to get access tokens for this user account
+def get_access_token(resource_key, resource_secret):
+    verifier = input('\nOAuth Verifier: ')
+    twitter = OAuth1Session(client_key=client_key,
+                            client_secret=client_secret,
+                            resource_owner_key=resource_key,
+                            resource_owner_secret=resource_secret)
+    url = 'https://api.twitter.com/oauth/access_token'
+    data = {"oauth_verifier": verifier}
+    access_token_response = twitter.post(url, data=data)
+    access_tokens_unparsed = access_token_response.text.split('&')
+    access_tokens_parsed = []
+    for x in access_tokens_unparsed:
+        access_tokens_parsed.append(x.split('=')[1])
+    return access_tokens_parsed
+
+
+def get_twitter_user_session(oauth_token, oauth_secret):
+    twitter = OAuth1Session(client_key=client_key,
+                            client_secret=client_secret,
+                            resource_owner_key=oauth_token,
+                            resource_owner_secret=oauth_secret)
+    return twitter
+
+
+# Execute all of the above methods in sequence to authenticate to Twitter
+def do_twitter_login():
+    keys = get_resource_token()
+    get_oauth_verifier(keys[0])
+    tokens = get_access_token(keys[0], keys[1])
+    return get_twitter_user_session(tokens[0], tokens[1])
 
 
 # The main widget for the main menu Kivy app
@@ -59,17 +114,8 @@ class MainWidget(Widget):
 
 # The main menu for the Kivy app
 class MainMenu(App):
-    # Creates a new OAuth V1.0 session for accessing Twitter
-    def get_session(self):
-        # Create OAuth V1.0 session
-        twitter = OAuth1Session(client_key=client_key,
-                                client_secret=client_secret,
-                                resource_owner_key=resource_owner_key,
-                                resource_owner_secret=resource_owner_secret)
-        return twitter
-
     def build(self):
-        tw_session = self.get_session()
+        tw_session = do_twitter_login()
         return MainWidget(tw_session)
 
 
